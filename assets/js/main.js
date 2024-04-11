a39d4aeed711968678dbdc5f23682f271731e2b6
Jolan Noirot <jolan.noirot@laposte.net>
2024-04-11 11:19:33 +0000

    ok age

--- assets/js/main.js
+++ assets/js/main.js
@@ -273,6 +273,7 @@
 function calculateAge(birthdate) {
   const today = new Date();
   const birthDate = new Date(birthdate);
+  console.log(birthdateSpan.textContent);
   let age = today.getFullYear() - birthDate.getFullYear();
   const month = today.getMonth() - birthDate.getMonth();
   if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
