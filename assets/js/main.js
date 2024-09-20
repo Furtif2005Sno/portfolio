50302a02fbf3a9af24d4aaa15078b35246c849b6
Jolan Noirot <jolan.noirot@laposte.net>
2024-09-20 12:24:13 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -269,27 +269,3 @@
   new PureCounter();
 
 })()
-// Fonction pour calculer l'âge à partir de la date de naissance
-function calculateAge(birthdate) {
-  const today = new Date();
-  const parts = birthdate.split(' ');
-  const monthNames = ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aout', 'Sep', 'Oct', 'Nov', 'Dec'];
-  const monthIndex = monthNames.indexOf(parts[1]);
-  const birthDate = new Date(`${parts[2]}-${monthIndex + 1}-${parts[0]}`);
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const month = today.getMonth() - birthDate.getMonth();
-  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
-  }
-  return age;
-}
-
-// Récupérer la balise span qui contient la date de naissance
-const birthdateSpan = document.getElementById('birthdate');
-// Récupérer la balise span qui va afficher l'âge calculé
-const ageSpan = document.getElementById('age');
-
-// Calculer l'âge au chargement de la page
-const age = calculateAge(birthdateSpan.textContent);
-// Afficher l'âge calculé
-ageSpan.textContent = age;
